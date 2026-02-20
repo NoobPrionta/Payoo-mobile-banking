91fc011a5132d089245be76762577da2d4d7061d
Prionta Saha Dip <pksaha500@gmail.com>
2026-02-20 22:03:37 +0600

    added dot dot

--- script/login.js
+++ script/login.js
@@ -17,7 +17,7 @@ document.getElementById('login-btn').addEventListener("click",function(){
         //3-1 true ::: >> alert>homepage
         alert("login-success");
 
-        window.location.assign("/home.html");
+        window.location.assign("../home.html");
 
     }else{
         //3-2 false ::: >> alert>return
